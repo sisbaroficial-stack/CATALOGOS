37724dc969fe16436953bc79c7326428eb4b4db2
sisbaroficial-stack <sisbaroficial@gmail.com>
2026-08-20 15:36:17 -0500

    Servir archivos multimedia con DEBUG desactivado

--- catalogo_app/urls.py
+++ catalogo_app/urls.py
@@ -1,12 +1,22 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
 ]
 
+# django.conf.urls.static.static() ignora rutas cuando DEBUG=False. Render usa
+# DEBUG=False incluso al habilitar SERVE_MEDIA, por eso declaramos esta ruta de
+# desarrollo/instancia única de forma explícita para los archivos subidos.
 if settings.DEBUG or settings.SERVE_MEDIA:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    media_url_path = settings.MEDIA_URL.lstrip('/')
+    urlpatterns += [
+        re_path(
+            rf'^{media_url_path}(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT},
+        ),
+    ]
